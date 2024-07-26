ff0462822f2a136b82e688881bff4c2fccd560b2
Vaibhav-cls <vaibhavghanekar19@gmail.com>
2024-07-26 09:45:16 +0530

    updated dependencies

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const MONGO_URL = "mongodb://127.0.0.1:27017/IncentiveBasedSystem";
+const path = require("path");
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
